391d2b83779c463e57723d2aff73057c04784592
Filip Åsberg <filip.asberg@gmail.com>
2022-12-01 22:40:59 +0100

    First commit

---
+++ Day1.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Day1 {
+	public static void main(String[] args) {
+		new Day1();
+	}
+
+	public Day1() {
+		ArrayList<String> input = ReadInput.read("res/input1.txt");
+		partOne(input);
+		partTwo(input);
+	}
+
+	private void partOne(ArrayList<String> input) {
+
+	}
+
+	private void partTwo(ArrayList<String> input) {
+
+	}
+
+}
