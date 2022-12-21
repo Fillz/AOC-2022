f6091cce7016a4eb92a91f4b5c3624b9155ae25d
Filip Åsberg <filip.asberg@arbetsformedlingen.se>
2022-12-21 19:32:26 +0100

    Completed day 21 part 2

--- Day21.java
+++ Day21.java
@@ -15,13 +15,77 @@ public class Day21 {
 	private void partOne(ArrayList<String> input) {
 		HashMap<String, Yell> monkeyYells = parseInput(input);
 		System.out.println(calculateNumber("root", monkeyYells));
-		System.out.println();
 	}
 
 	private void partTwo(ArrayList<String> input) {
 		HashMap<String, Yell> monkeyYells = parseInput(input);
-		System.out.println(calculateNumber("root", monkeyYells));
-		System.out.println();
+		calculateNumber("root", monkeyYells);
+		YellOperation rootYell = (YellOperation) monkeyYells.get("root");
+
+		long soughtValue;
+		String startMonkey;
+		if(rootYell.humanSide.equals("left")) {
+			soughtValue = calculateNumber(rootYell.right, monkeyYells);
+			startMonkey = rootYell.left;
+		}
+		else {
+			soughtValue = calculateNumber(rootYell.left, monkeyYells);
+			startMonkey = rootYell.right;
+		}
+
+		long res = calculateHumanYell(startMonkey, soughtValue, monkeyYells);
+		System.out.println(res);
+
+	}
+
+	private long calculateHumanYell(String currentMonkeyName, long currentValue, HashMap<String, Yell> monkeyYells) {
+		YellOperation currentMonkey = (YellOperation) monkeyYells.get(currentMonkeyName);
+		if(currentMonkey.humanSide.equals("left")) {
+			long rightValue = calculateNumber(currentMonkey.right, monkeyYells);
+			if(currentMonkey.operation == '+') {
+				if(monkeyYells.get(currentMonkey.left) instanceof YellNumber)
+					return currentValue - rightValue;
+				return calculateHumanYell(currentMonkey.left, currentValue - rightValue, monkeyYells);
+			}
+			else if(currentMonkey.operation == '-') {
+				if(monkeyYells.get(currentMonkey.left) instanceof YellNumber)
+					return currentValue + rightValue;
+				return calculateHumanYell(currentMonkey.left, currentValue + rightValue, monkeyYells);
+			}
+			else if(currentMonkey.operation == '*') {
+				if(monkeyYells.get(currentMonkey.left) instanceof YellNumber)
+					return currentValue / rightValue;
+				return calculateHumanYell(currentMonkey.left, currentValue / rightValue, monkeyYells);
+			}
+			else {
+				if(monkeyYells.get(currentMonkey.left) instanceof YellNumber)
+					return currentValue * rightValue;
+				return calculateHumanYell(currentMonkey.left, currentValue * rightValue, monkeyYells);
+			}
+		}
+		else {
+			long leftValue = calculateNumber(currentMonkey.left, monkeyYells);
+			if(currentMonkey.operation == '+') {
+				if(monkeyYells.get(currentMonkey.right) instanceof YellNumber)
+					return currentValue - leftValue;
+				return calculateHumanYell(currentMonkey.right, currentValue - leftValue, monkeyYells);
+			}
+			else if(currentMonkey.operation == '-') {
+				if(monkeyYells.get(currentMonkey.right) instanceof YellNumber)
+					return currentValue + leftValue;
+				return calculateHumanYell(currentMonkey.right, leftValue - currentValue, monkeyYells);
+			}
+			else if(currentMonkey.operation == '*') {
+				if(monkeyYells.get(currentMonkey.right) instanceof YellNumber)
+					return currentValue / leftValue;
+				return calculateHumanYell(currentMonkey.right, currentValue / leftValue, monkeyYells);
+			}
+			else {
+				if(monkeyYells.get(currentMonkey.right) instanceof YellNumber)
+					return leftValue / currentValue;
+				return calculateHumanYell(currentMonkey.right, leftValue / currentValue, monkeyYells);
+			}
+		}
 	}
 
 	private long calculateNumber(String currentMonkey, HashMap<String, Yell> monkeyYells) {
@@ -69,7 +133,7 @@ public class Day21 {
 				map.put(name, new YellOperation(operation, left, right));
 			}
 			else {
-				YellNumber yn = new YellNumber(Integer.parseInt(s));
+				YellNumber yn = new YellNumber(Long.parseLong(s));
 				if(name.equals("humn"))
 					yn.isHuman = true;
 
